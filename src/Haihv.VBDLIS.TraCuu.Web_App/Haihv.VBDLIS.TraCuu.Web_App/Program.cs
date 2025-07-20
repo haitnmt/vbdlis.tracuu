ca98baeb4260ba7606638267e85859fa26bcc193
admin <admin@haihv.vn>
2025-07-20 10:59:52 +0700

    feat: khởi tạo dự án Aspire với Web API và Blazor
    Khởi tạo cấu trúc dự án bao gồm Web API, Blazor Web App, AppHost và ServiceDefaults.
    Thêm các cấu hình health check, OpenTelemetry, và service discovery mặc định cho các dịch vụ.
    Thiết lập các file cấu hình chạy, logging và giao diện mẫu cho ứng dụng web.

---
+++ src/Haihv.VBDLIS.TraCuu.Web_App/Haihv.VBDLIS.TraCuu.Web_App/Program.cs
@@ -0,0 +1,40 @@
+using Haihv.VBDLIS.TraCuu.Web_App.Components;
+using TraCuu.ServiceDefaults;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Default health checks
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Haihv.VBDLIS.TraCuu.Web_App.Client._Imports).Assembly);
+
+app.Run();
